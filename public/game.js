d73ac2355b344d5d2db79850b829074ed404b924
DIY-It-All <1983.tanu.gupta@gmail.com>
2024-03-21 21:59:03 +0530

    gravity ded

--- public/game.js
+++ public/game.js
@@ -1,199 +1,156 @@
 import * as THREE from "three";
-
 import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls.js";
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js'
-
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
 import * as CANNON from "cannon-es";
-const renderer = new THREE.WebGLRenderer({
-  antialias: true,
-});
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement);
-
-window.addEventListener(
-  "resize",
-  () => {
-    this._OnWindowResize();
-  },
-  false
-);
-
-const fov = 60;
-const aspect = 1920 / 1080;
-const near = 1.0;
-const far = 5000.0;
-let camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.set(75, 20, 0);
-
-let scene = new THREE.Scene();
-let world = new CANNON.World({
-  gravity: new CANNON.Vec3(0, -9.81, 0),
-});
-let light = new THREE.AmbientLight(0x101010);
-scene.add(light);
-
-const hemlight = new THREE.HemisphereLight( 0xffffbb, 0x080820, 10 );
-scene.add( hemlight );
-
-
-const controls = new PointerLockControls(camera, document.body);
-document.getElementById("playBtn").onclick = () => {
-  controls.lock();
+var camera, scene, renderer, mesh, goal, follow, world, clock, meshPhy;
+var box, floor;
+var boxPhy, floorPhy;
+var keys = {
+  a: false,
+  s: false,
+  d: false,
+  w: false,
+  space: false,
 };
+var dt = 1;
+var time = 0;
+var newPosition = new THREE.Vector3();
+var matrix = new THREE.Matrix4();
+var mixers = [];
+var walking;
+var stop = 1;
+var DEGTORAD = 0.01745327;
+var temp = new THREE.Vector3();
+var dir = new THREE.Vector3();
+var a = new THREE.Vector3();
+var b = new THREE.Vector3();
+var coronaSafetyDistance = 20;
+var velocity = 0.0;
+var speed = 0.0;
 
-const loader = new THREE.CubeTextureLoader();
-const texture = loader.load([
-  "./resources/posx.jpg",
-  "./resources/negx.jpg",
-  "./resources/posy.jpg",
-  "./resources/negy.jpg",
-  "./resources/posz.jpg",
-  "./resources/negz.jpg",
-]);
-// scene.background = texture;
-scene.fog = new THREE.Fog( 0xcccccc, 10, far );
-const plane = new THREE.Mesh(
-  new THREE.PlaneGeometry(100, 100, 10, 10),
-  new THREE.MeshStandardMaterial({
-    color: 0xffffff,
-  })
-);
-plane.castShadow = false;
-plane.receiveShadow = true;
-plane.rotation.x = -Math.PI / 2;
-scene.add(plane);
-var slipperyMaterial = new CANNON.Material("slipperyMaterial");
+init();
 
-// The ContactMaterial defines what happens when two materials meet.
-// In this case we want friction coefficient = 0.0 when the slippery material touches ground.
-var slippery_ground_cm = new CANNON.ContactMaterial(
-  slipperyMaterial,
-  slipperyMaterial,
-  {
-    friction: 0.01,
-    restitution: 0.3,
-    contactEquationStiffness: 1e8,
-    contactEquationRelaxation: 3,
-  }
-);
-
-// We must add the contact materials to the world
-world.addContactMaterial(slippery_ground_cm);
-
-let planeBod = new CANNON.Body({
-  shape: new CANNON.Plane(),
-  mass: 0,
-  material: slipperyMaterial
+function init() {
+  clock = new THREE.Clock();
+  camera = new THREE.PerspectiveCamera(
+      60,
+      window.innerWidth / window.innerHeight,
+      0.01,
+      1000
+      );
+  camera.position.set(0, 20, 0);
+  world = new CANNON.World({
+    gravity: new CANNON.Vec3(0, -10, 0),
 });
-console.log(planeBod);
-planeBod.quaternion.copy(plane.quaternion);
-planeBod.position.copy(plane.position);
-world.addBody(planeBod);
-
-
-
-
-
-const fbxLoader = new FBXLoader()
-fbxLoader.load(
-    './resources/island2/source/Stronghold.fbx',
-    (object) => {
-        scene.add(object)
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log(error)
-    }
-)
-
-fbxLoader.load(
-    './resources/island2/source/Stronghold.fbx',
-    (object) => {
-        scene.add(object)
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log(error)
-    }
-)
-
-
-
-
-
-
-const box = new THREE.Mesh(
-  new THREE.SphereGeometry(),
-  new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-  })
-);
-box.position.set(0, 500, 0);
-box.castShadow = true;
-box.receiveShadow = true;
-scene.add(box);
-let boxBod = new CANNON.Body({
-  shape: new CANNON.Sphere(),
-  mass: 1,
-  material: slipperyMaterial
+scene = new THREE.Scene();
+camera.lookAt(scene.position);
+let loader = new FBXLoader();
+loader.load("./resources/island2/source/Stronghold.fbx", (model) => {
+    model.scale.set(0.5, 0.5, 0.5);
+    // scene.add(model);
 });
-boxBod.quaternion.copy(box.quaternion);
-boxBod.position.copy(box.position);
-world.addBody(boxBod);
-
-animate();
-
-function OnWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-addEventListener("resize", OnWindowResize);
-var keyMap = {};
-const onDocumentKey = (e) => {
-  keyMap[e.code] = e.type === "keydown";
-};
-document.addEventListener("keydown", onDocumentKey, false);
-document.addEventListener("keyup", onDocumentKey, false);
-
-var v = new THREE.Vector3(0, 0, 0);
-function animate() {
-  world.step(1 / 60);
-  requestAnimationFrame(() => {
-    controls.getDirection(v);
-    if (keyMap["KeyW"]) {
-        boxBod.position.z += v.z;
-        boxBod.position.x += v.x;
-    //   boxBod.applyImpulse(new CANNON.Vec3(v.x, 0, v.y));
-    }
-    if (keyMap["KeyA"]) {
-      boxBod.position.x += v.z;
-      boxBod.position.z += v.x;
-    }
-    if (keyMap["KeyS"]) {
-      boxBod.position.z -= v.z;
-      boxBod.position.x -= v.x;
-    }
-    if (keyMap["KeyD"]) {
-      boxBod.position.x -= v.z;
-      boxBod.position.z -= v.x;
-    }
-    camera.position.copy(boxBod.position);
-    camera.position.add(v.multiply(new THREE.Vector3(-10, -10, -10)));
-    box.quaternion.copy(boxBod.quaternion);
-    box.position.copy(boxBod.position);
-
-    plane.quaternion.copy(planeBod.quaternion);
-    plane.position.copy(planeBod.position);
-    renderer.render(scene, camera);
+loader.load("./resources/player/Aj.fbx", (model) => {
+    model.scale.set(0.05, 0.05, 0.05);
+    mesh = model;
+    meshPhy = new CANNON.Body({
+        shape: new CANNON.Cylinder(),
+        mass: 1,
+    });
+    meshPhy.position.copy(mesh.position);
+    world.addBody(meshPhy);
+    goal = new THREE.Object3D();
+    follow = new THREE.Object3D();
+    follow.position.z = -coronaSafetyDistance;
+    mesh.add(follow);
+    goal.add(camera);
+    scene.add(mesh);
+    mesh.position.set(-21, 0, 152);
+    
+    box = new THREE.Mesh(
+        new THREE.BoxGeometry(150, 150, 150),
+      new THREE.MeshPhongMaterial({ color: 0x00f0f0 })
+      );
+      scene.add(box);
+      boxPhy = new CANNON.Body({
+          shape: new CANNON.Box(new CANNON.Vec3(150, 150, 150)),
+          mass: 0,
+        });
+        boxPhy.position.copy(box.position);
+        world.addBody(boxPhy);
+        floor = new THREE.Mesh(
+          new THREE.BoxGeometry(1500, 10, 1500),
+          new THREE.MeshPhongMaterial({ color: 0x00f0f0 })
+        );
+        floor.position.y -= 7;
+        scene.add(floor);
+        floorPhy = new CANNON.Body({
+          shape: new CANNON.Box(new CANNON.Vec3(1500, 3, 1500)),
+          mass: 0,
+        });
+        floorPhy.position.copy(floor.position);
+        world.addBody(floorPhy);
+        
+        let anim_loader = new FBXLoader();
+    anim_loader.load(
+      "./resources/player/anims/Standard Walk.fbx",
+      (animation) => {
+        let m = new THREE.AnimationMixer(mesh);
+        mixers.push(m);
+        walking = m.clipAction(animation.animations[0]);
+        walking.play();
+      }
+    );
     animate();
   });
+  var gridHelper = new THREE.GridHelper(40, 40);
+  scene.add(gridHelper);
+
+  scene.add(new THREE.AxesHelper());
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const light = new THREE.HemisphereLight(0xffffbb, 0x080820, 50);
+  scene.add(light);
+
+  document.body.addEventListener("keydown", function (e) {
+    const key = e.code.replace("Key", "").toLowerCase();
+    if (keys[key] !== undefined) keys[key] = true;
+  });
+  document.body.addEventListener("keyup", function (e) {
+    const key = e.code.replace("Key", "").toLowerCase();
+    if (keys[key] !== undefined) keys[key] = false;
+  });
+}
+function animate() {
+  dt = clock.getDelta();
+//   requestAnimationFrame(animate);
+  world.step(1 / 60);
+  mixers.map((m) => m.update(dt));
+  speed = 0.0;
+  if (keys.w) speed = 0.1;
+  else if (keys.s) speed = -0.1;
+  if (keys.space) mesh.translateY(0.01);
+  velocity += (speed - velocity) * 0.3;
+  mesh.translateZ(velocity);
+
+  if (keys.a) mesh.rotateY(0.05);
+  else if (keys.d) mesh.rotateY(-0.05);
+
+  a.lerp(mesh.position, 0.4);
+  b.copy(goal.position);
+
+  dir.copy(a).sub(b).normalize();
+  const dis = a.distanceTo(b) - coronaSafetyDistance;
+  goal.position.addScaledVector(dir, dis);
+  goal.position.lerp(temp, 0.02);
+  temp.setFromMatrixPosition(follow.matrixWorld);
+  camera.lookAt(mesh.position);
+  mesh.position.copy(meshPhy.position);
+  box.position.copy(boxPhy.position);
+  box.quaternion.copy(boxPhy.quaternion);
+
+  renderer.render(scene, camera);
+  setInterval(animate,1000)
 }
